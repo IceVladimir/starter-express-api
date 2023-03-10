298b4f6f4e62e358fce61b7a5698a1aee4fc5c15
IceVladimir <74771948+IceVladimir@users.noreply.github.com>
2023-03-10 13:10:00 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
 const express = require('express')
 const app = express()
+var token = ""
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+    res.json(["Tony","Lisa","Michael","Ginger","Food"]);
 })
+
+app.post('/api', function(req, res) {
+
+
+(async() => {
+    if (req.body.auth == true){
+    characterAI.unauthenticate()
+    token = await characterAI.authenticateAsGuest();
+    }
+    const characterId = "v3lyisRb7INyd5BUdUKEKS1-MUTBom9dY9qV9-2ioTE"
+    const chat = await characterAI.createOrContinueChat(characterId);
+    const response = await chat.sendAndAwaitResponse(req.body.msg, true)
+
+  res.send({
+    'Answer': response.text,
+  });
+})();
+
+});
+
 app.listen(process.env.PORT || 3000)
