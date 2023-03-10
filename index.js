3ada2ebe2628f564eca58e305a285ad5bd151392
IceVladimir <fikfi12s12@gmail.com>
2023-03-10 13:50:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,40 +1,30 @@
-var program = require('commander');
-var exec = require('child_process').exec;
+const express = require('express')
+const app = express()
+var token = ""
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-var run = function(cmd){
-  var child = exec(cmd, function (error, stdout, stderr) {
-    if (stderr !== null) {
-      console.log('' + stderr);
+app.get('/', (req, res) => {
+    res.json(["Tony","Lisa","Michael","Ginger","Food"]);
+})
+
+app.post('/', function(req, res) {
+
+
+(async() => {
+    if (req.body.auth == true){
+    characterAI.unauthenticate()
+    token = await characterAI.authenticateAsGuest();
     }
-    if (stdout !== null) {
-      console.log('' + stdout);
-    }
-    if (error !== null) {
-      console.log('' + error);
-    }
+    const characterId = "v3lyisRb7INyd5BUdUKEKS1-MUTBom9dY9qV9-2ioTE"
+    const chat = await characterAI.createOrContinueChat(characterId);
+    const response = await chat.sendAndAwaitResponse(req.body.msg, true)
+
+  res.send({
+    'Answer': response.text,
   });
-};
+})();
 
-program
-  .version('0.1.3')
-  .option('i, --install ', 'install packages')
-  .parse(process.argv);
-
-
-
-if (program.install) {
-  run('npm install node_characterai');
-}
-
-
-var count = 0;
-
-
-// If parameter is missing or not supported, display help
-program.options.filter(function (option) {
-  if(!(option.short == process.argv[2]))
-    count++
 });
 
-if(count == program.options.length)
-  program.help();
+app.listen(process.env.PORT || 3000)
